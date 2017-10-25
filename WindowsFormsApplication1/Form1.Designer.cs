2187a96266a0d8794db6177cff7b4b3358de4275
joerg.matthes <joerg.matthes@kit.edu>
2017-10-25 10:58:48 +0200

    rt

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(297, 269);
+            this.ClientSize = new System.Drawing.Size(337, 289);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
